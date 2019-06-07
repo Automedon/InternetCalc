17f656f6ff365af6ccf663779e503fe70c4ff288
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-06-07 13:33:25 +0700

    Add files via upload

---
+++ precache-manifest.8b98ff95c32fdb08633195a096204184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df9a27c5c7569e546f7aba146f9474f",
+    "url": "/InternetCalc/index.html"
+  },
+  {
+    "revision": "b1c0291563eba9ec06c5",
+    "url": "/InternetCalc/static/css/main.ab083fc5.chunk.css"
+  },
+  {
+    "revision": "325c84dd89d02bdca26c",
+    "url": "/InternetCalc/static/js/2.27d8b0f8.chunk.js"
+  },
+  {
+    "revision": "b1c0291563eba9ec06c5",
+    "url": "/InternetCalc/static/js/main.75718526.chunk.js"
+  },
+  {
+    "revision": "20b6a820f30e34e568a7",
+    "url": "/InternetCalc/static/js/runtime~main.a75b8adc.js"
+  }
+]);
